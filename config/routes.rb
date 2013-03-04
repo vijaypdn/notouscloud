c1f5eef85016da484c424a73c94c99b3bb831007
vijay reddy <vijaypdn@gmail.com>
2013-03-04 14:50:18 +0530

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 SinglyRailsSkeleton::Application.routes.draw do
-  get "signup/user"
-
-  get "sig/sign"
-
-  get "signup/sign"
-
-  get "signup/signup"
-
   match "auth/:service"          => "auth#service"
   match "auth/:service/callback" => "auth#callback"
   match "logout"                 => "auth#logout"
--- notouscloud/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Notouscloud::Application.config.secret_token = 'b639a16a16f06e307b17a4466c6421919cb826bb03764f2fd60337c1f239ffc37b0757d1a64b0c5835607a0fc2a0c40bdc2e11c8fe838ef17a4c087f114d218f'
